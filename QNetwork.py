0bfda94a1e796e7ad64f5640fbb6ee8bca0a364b
rs-benatti <rafa.senna.benatti2@gmail.com>
2023-11-24 01:16:44 +0100

    correction in comments

--- QNetwork.py
+++ QNetwork.py
@@ -6,12 +6,12 @@ import torch.nn as nn
 class QNetwork(nn.Module):
     def __init__(self):
         super(QNetwork,self).__init__()
-        self.conv1 = nn.Conv2d(3, 8, kernel_size=4, stride=2) # 3 * 96 * 96
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=2) # 32 * 47 * 47
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=2) # 64 * 22 * 22
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=2) # 128 * 10 * 10
-        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, stride=1) # 256 * 6 * 6
-        self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=1) # 256 * 6 * 6
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=4, stride=2) 
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=2) 
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=2) 
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=2) 
+        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, stride=1) 
+        self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=1) 
 
         self.fc1= nn.Linear(256 * 1 * 1, 100) 
         self.fc2 = nn.Linear(100,100)
